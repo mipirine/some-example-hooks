c0bae794e79289516f5237f90488b47fa79673dd
Mirja Pirinen <mirja.pirinen@hotmail.com>
2024-04-23 11:50:00 +0300

    more advanced hooks

---
+++ src/components/UseCallBackExample.jsx
@@ -0,0 +1,30 @@
+/**
+ * useCallback is handy if you have performance issues 
+ * or slow/expensive functions 
+ */
+import React, {useState, useCallback} from 'react'
+
+function UseCallBackExample() {
+    const [tasks, setTasks] = useState([])
+
+    const addTask = useCallback(() => {
+        setTasks((prevState) => [...prevState, 'Some Task'])
+    }, [setTasks])
+
+  return (
+    <div>
+      <Button addTask={addTask}/>
+      {tasks.map((task, index) => (
+        <p key={index}>{task}</p>
+      ))}
+    </div>
+  )
+}
+
+const Button = React.memo(({addTask}) => {
+    console.log('Button rendered')
+    return <div>
+        <button className='btn btn-primary' onClick={addTask}>Add Task</button>
+    </div>
+})
+export default UseCallBackExample
